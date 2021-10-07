31b201fef952db81865788ccc0d39ec9c6001ef1
misael-reyes <misaelstark11@gmail.com>
2021-10-07 09:16:44 -0500

    se sube el proyecto inicial

---
+++ database/migrations/2021_10_06_231607_categorias.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Categorias extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('categorias', function (Blueprint $table) {
+            /**
+             * InnoDB nos va a permitir eliminar en cascada. Por ejemplo cuando eliminamos Drama
+             * de la tabla categorias, se eliminarán todos los libros que tengan una relación
+             * con la categoria Drama
+             */
+            $table->engine="InnoDB";
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
